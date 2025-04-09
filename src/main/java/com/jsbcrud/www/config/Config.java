bf94e96069c7045fb37a0345834e958e639d23d2
AndreyHP <andreymarins501@gmail.com>
2025-04-09 19:44:58 -0300

    update config

--- src/main/java/com/jsbcrud/www/config/Config.java
+++ src/main/java/com/jsbcrud/www/config/Config.java
@@ -18,51 +18,14 @@ import java.time.LocalDate;
 @Getter
 public class Config {
 
-    /**
-     * Nome da aplicação.
-     * Este valor é carregado a partir das propriedades de configuração.
-     */
     private final String name;
-
-    /**
-     * Nome do cabeçalho da aplicação.
-     * Este valor é carregado a partir das propriedades de configuração.
-     */
     private final String headerName;
-
-    /**
-     * Nome curto da aplicação.
-     * Este valor é carregado a partir das propriedades de configuração.
-     */
     private final String shortName;
-
-    /**
-     * Ano base da aplicação, geralmente o ano de início.
-     * Este valor é carregado a partir das propriedades de configuração.
-     */
     private final int year;
-
-    /**
-     * Texto de copyright, com um marcador de ano que será substituído pelo ano atual.
-     * Este valor é carregado a partir das propriedades de configuração.
-     */
     private final String copyright;
-
-    /**
-     * URL ou caminho do logo da aplicação.
-     * Este valor é carregado a partir das propriedades de configuração.
-     */
     private final String logo;
-
-    /**
-     * Método responsável por calcular e retornar o texto de copyright, substituindo o marcador de ano
-     * pela data correta. Se o ano atual for maior que o ano base, ele substituirá o marcador "[YEAR]"
-     * com a faixa de anos (exemplo: "2023 - 2025"), caso contrário, ele substituirá com o ano atual.
-     *
-     * @return O texto de copyright com o ano substituído.
-     */
-
     private final int cookieHourslife = 48;
+
     public String getCopyright() {
         int currentYear = LocalDate.now().getYear();
         return currentYear > year
