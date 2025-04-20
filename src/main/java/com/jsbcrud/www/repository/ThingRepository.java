869139b453b5abf8304c2739ea767dee2052074b
Hugo <hugoazevedo649@gmail.com>
2025-04-20 09:01:39 -0300

    thing com problemas

--- src/main/java/com/jsbcrud/www/repository/ThingRepository.java
+++ src/main/java/com/jsbcrud/www/repository/ThingRepository.java
@@ -4,7 +4,9 @@ import com.jsbcrud.www.model.Thing;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ThingRepository extends JpaRepository<Thing, Long> {
     List<Thing> findByStatusOrderByDateDesc(Thing.Status status);
+    Optional<Thing> findByIdAndStatus(Integer id, Thing.Status status);
 }
